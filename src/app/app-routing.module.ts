15bc0734df6eb410c7aecc39f62bb5871a96ffc5
pos liberado <pospos@labcomp.usc>
2019-09-14 10:40:59 -0300

    Add flashligh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'flashlight', loadChildren: './flashlight/flashlight.module#FlashlightPageModule' }
 ];
 
 @NgModule({
